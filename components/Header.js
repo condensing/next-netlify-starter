2cb1d5592fe48ec73f166c7efd949fd65bf3c748
condensing <89842133+condensing@users.noreply.github.com>
2025-05-14 01:19:24 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,3 @@
 <header className="relative">
-  <img src="/logo.png" alt="Logo" className="absolute top-4 right-4 w-20" />
+  <img src="public/logo.png" alt="Logo" className="absolute top-4 right-4 w-20" />
 </header>
