79f55ac2aa80e0b868f9633d84aeff02224545cc
condensing <89842133+condensing@users.noreply.github.com>
2025-05-14 01:04:50 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
+    <div className="footer">
     <footer className="text-center py-4 bg-gray-100 text-gray-700 text-sm">
       &copy; 2025 SWRP. All rights reserved.
     </footer>
