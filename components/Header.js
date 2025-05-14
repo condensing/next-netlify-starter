f211537145b45bb92071c4cc052c8acc2adbf7bc
condensing <89842133+condensing@users.noreply.github.com>
2025-05-14 01:25:23 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,4 @@
     alt="Logo"
     className="absolute top-4 right-4 w-20 h-auto"
   />
-  <h1 className={styles.title}>SW Realty Partners</h1>
-  <p className={styles.description}>Your future, our priority.</p>
 </header>
