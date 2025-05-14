a1014b658ccb99f383b43806d913e148ce8b86e8
condensing <89842133+condensing@users.noreply.github.com>
2025-05-13 22:55:02 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>SWRP</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my SW Realty Partners" />
+        <Header title="SW Realty Partners" />
         <p className="description">
-          Website is currently under development <code>- SW Realty Team</code>
+          Website is currently under development <code>SWRP</code>
         </p>
       </main>
 
