556835b5db1f78efe7aa23d7087762349ad30268
condensing <89842133+condensing@users.noreply.github.com>
2025-05-14 01:05:40 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,14 @@
-import styles from './Footer.module.css'
-
+import styles from './Footer.module.css';
 import React from 'react';
 
 const Footer = () => {
   return (
     <div className="footer">
-    <footer className="text-center py-4 bg-gray-100 text-gray-700 text-sm">
-      &copy; 2025 SWRP. All rights reserved.
-    </footer>
+      <footer className="text-center py-4 bg-gray-100 text-gray-700 text-sm">
+        &copy; 2025 SWRP. All rights reserved.
+      </footer>
+    </div>
   );
 };
 
 export default Footer;
-
