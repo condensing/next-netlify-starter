74c03279c940523b9462a0431273f58b58e3667e
condensing <89842133+condensing@users.noreply.github.com>
2025-05-14 01:20:30 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,9 @@
-<header className="relative">
-  <img src="public/logo.png" alt="Logo" className="absolute top-4 right-4 w-20" />
+<header className="relative w-full">
+  <img
+    src="/logo.png"
+    alt="Logo"
+    className="absolute top-4 right-4 w-20 h-auto"
+  />
+  <h1 className={styles.title}>SW Realty Partners</h1>
+  <p className={styles.description}>Your future, our priority.</p>
 </header>
